b59e97fec08125b725831b6b17699e9e15112e1e
Greg Franko <gfranko5@yahoo.com>
2012-09-12 13:45:58 -0400

    Updated Custom Download Page Docs

--- grunt.js
+++ grunt.js
@@ -1,11 +1,27 @@
 module.exports = function(grunt) {
 
-  grunt.registerTask('default', 'qunit');
+  var min = {};
+  [
+    'Collection',
+    'Events',
+    'Model',
+    'Router',
+    'Sync',
+    'View'
+  ].forEach(function(name) {
+    min[name] = {
+      src: 'customBuild/js/Backbone.' + name + '.js',
+      dest: 'customBuild/js/Backbone.' + name + '.min.js'
+    };
+  });
 
   grunt.initConfig({
     qunit: {
       index: 'test/index.html'
-    }
+    },
+    min: min
   });
+
+  grunt.registerTask('default', 'min');
 
 };
